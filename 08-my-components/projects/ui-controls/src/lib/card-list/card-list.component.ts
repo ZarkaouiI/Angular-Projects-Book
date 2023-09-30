a24c74c2ffbbc3117c6f6b57fdee29bbfd0fd718
zarkaouii <zarkaoui99@gmail.com>
2023-09-30 16:08:55 +0100

    chapter 7 and 8

---
+++ 08-my-components/projects/ui-controls/src/lib/card-list/card-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Card } from '../card';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'lib-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css']
+})
+export class CardListComponent {
+
+  @Input() cards: Card[] = [];
+  @Output() cardChange = new EventEmitter<Card[]>();
+
+  sortCards(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
+    this.cardChange.emit(this.cards);
+  }
+
+}
